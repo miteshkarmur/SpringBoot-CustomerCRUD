1a7fa58c1430fd85799a46d82d77700847d4d6f6
Mitesh <mitesh.karmur@infosys.com>
2018-10-05 16:06:26 +0530

    final commit

--- SpringBoot-CustomerCRUD/src/main/java/com/miteshkarmur/SpringBootCustomerCRUD/controller/MainController.java
+++ SpringBoot-CustomerCRUD/src/main/java/com/miteshkarmur/SpringBootCustomerCRUD/controller/MainController.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.miteshkarmur.SpringBootCustomerCRUD.entity.Customer;
 import com.miteshkarmur.SpringBootCustomerCRUD.service.CustomerService;
-
 
 @Controller
 //@RequestMapping("/")
@@ -19,10 +21,10 @@ public class MainController {
 	CustomerService customerService;
 	
 	@GetMapping("/")
-	public String addCustomers(Model model) {
-		customerService.saveOrUpdateCustomer(new Customer("Mitesh", "Keshur", "mk@gmail.com"));
-		customerService.saveOrUpdateCustomer(new Customer("Subhash", "Keshur", "sk@gmail.com"));
-		customerService.saveOrUpdateCustomer(new Customer("Payal", "Keshur", "pk@gmail.com"));
+	public String myCustomers(Model model) {
+		customerService.saveOrUpdateCustomer(new Customer("Mitesh", "Karmur", "mk@gmail.com"));
+		customerService.saveOrUpdateCustomer(new Customer("Subhash", "Karmur", "sk@gmail.com"));
+		customerService.saveOrUpdateCustomer(new Customer("Payal", "Karmur", "pk@gmail.com"));
 		return "index";
 	}
 	@GetMapping("/listCustomers")
@@ -32,4 +34,30 @@ public class MainController {
 		return "list-customers";
 	}
 	
+	@GetMapping("/addCustomerForm")
+	public String addCustomerForm(Model model) {
+		Customer customer=new Customer();
+		model.addAttribute("customer",customer);
+		return "customer-form";
+	}
+	
+	@GetMapping("/updateCustomerForm")
+	public String updateCustomerform(@RequestParam("custId") Integer custId,Model model) {
+		Customer customer=customerService.getCustomerById(custId);
+		model.addAttribute("customer",customer);
+		return "customer-form";
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer customer) {
+		customerService.saveOrUpdateCustomer(customer);
+		return "redirect:/listCustomers";
+	}
+	
+	@GetMapping("/deleteCustomer")
+	public String deleteCustomer(@RequestParam("custId") Integer custId) {
+		Customer customer=customerService.getCustomerById(custId);
+		customerService.deleteCustomer(customer);
+		return "redirect:/listCustomers";
+	}
 }
